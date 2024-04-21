b72c283102b37154f447bb24d028e60a4373c876
mertmtlu <mertmtl0109@gmail.com>
2024-04-21 16:49:48 +0300

    changing Type to eType

--- CE481 hw2/LongBar.cs
+++ CE481 hw2/LongBar.cs
@@ -11,17 +11,20 @@ namespace CE481_hw2
         // Section origin is left bottom corner
 
         #region
-        public LongBar(Type type, double conCover, double secHeight, double uStrain, double uStrength, double yStrain, double yStrenght)
+        public LongBar(eType type, double conCover, double secHeight, double uStrain, double uStrength, double yStrain, double yStrenght)
         {
             BarType = type; UltimateStrain = uStrain; UltimateStrength = uStrength; YieldStrain = yStrain; YieldStrength = yStrenght;
 
-            //if (BarType == Top)
+            if (BarType == eType.TopSteel)
+            {
+
+            }
 
         }
         #endregion
 
         #region Private Fields
-        private Type _BarType;
+        private eType _BarType;
         private double _Location;
         private double _UltimateStrain;
         private double _UltimateStrength;
@@ -34,7 +37,7 @@ namespace CE481_hw2
 
         #region Public Properties
 
-        public Type BarType { get => _BarType; set => _BarType = value; }
+        public eType BarType { get => _BarType; set => _BarType = value; }
         public double Location { get => _Location; set => _Location = value; }
         public double UltimateStrain { get => _UltimateStrain; set => _UltimateStrain = value; }
         public double UltimateStrength { get => _UltimateStrength; set => _UltimateStrength = value; }
@@ -45,7 +48,7 @@ namespace CE481_hw2
 
         #endregion
 
-        public enum Type
+        public enum eType
         {
             BottomSteel,
             TopSteel,
